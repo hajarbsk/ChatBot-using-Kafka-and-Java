81ca404d4987dfd0fd63965f674c4eef65531d4d
hajarbsk <hajarbousaken20@gmail.com>
2024-11-22 10:46:23 +0100

    add ressources

--- src/main/java/com/chatbot/chatbotapp/Main.java
+++ src/main/java/com/chatbot/chatbotapp/Main.java
@@ -2,9 +2,15 @@ package com.chatbot.chatbotapp;
 
 import com.chatbot.chatbotapp.consumer.KafkaConsumerExample;
 import com.chatbot.chatbotapp.producer.KafkaProducerExample;
+import com.chatbot.chatbotapp.server.ServerManager;
 
 public class Main {
     public static void main(String[] args) {
+        //Thread pour demarrer kafka et producer
+        new Thread(() -> {
+            ServerManager serverManager = new ServerManager();
+            serverManager.main(); //
+        }).start();
         // Thread pour démarrer le Producer
         new Thread(() -> {
             KafkaProducerExample producer = new KafkaProducerExample();
