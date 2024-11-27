2e05cddc1ddc46f70caccf2fd6b0e700a823d32b
hajarbsk <hajarbousaken20@gmail.com>
2024-11-27 10:16:34 +0100

    Sauvegarde du travail pour version-hajar

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,10 +3,13 @@ module com {
     requires javafx.controls;
     requires javafx.fxml;
     requires kafka.clients;
+    requires com.fasterxml.jackson.databind; // Pour Jackson
+
 
     opens com.GUI to javafx.fxml; // Pour le binding avec FXML
     exports com.Kafka;
 
     exports com.Traitement;
     exports com.GUI;
+    exports com;
 }
